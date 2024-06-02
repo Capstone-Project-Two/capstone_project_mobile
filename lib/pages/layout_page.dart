de2e47b8d02d71c166a1ab273edb558ec0aa142c
Your Name <youremail@example.com>
2024-06-03 00:15:35 +0700

    fix app bar

--- lib/pages/layout_page.dart
+++ lib/pages/layout_page.dart
@@ -27,6 +27,14 @@ class _LayoutPageState extends State<LayoutPage> {
     const ProfilePage(),
   ];
 
+  List<String> titles = [
+    'Home',
+    'Forum',
+    'Therapists',
+    'Resources',
+    'Profile',
+  ];
+
   void _navigateBottomBar(int index) {
     setState(() {
       _selectedIndex = index;
@@ -36,7 +44,18 @@ class _LayoutPageState extends State<LayoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(context),
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(200.0), // Set the height of the AppBar
+      //   child: ClipRRect(
+      //     borderRadius: BorderRadius.vertical(
+      //       bottom: Radius.circular(20.0), // Adjust the radius as needed
+      //     ),
+      //     child: _buildAppBar(context, titles[_selectedIndex]),
+      //   ),
+      // ),
+
+      appBar: _buildPreferredSize(context, titles[_selectedIndex]),
+
       body: FutureBuilder(
         future: rootBundle.loadString('.env'),
         initialData: '',
@@ -50,14 +69,67 @@ class _LayoutPageState extends State<LayoutPage> {
   }
 }
 
-AppBar _buildAppBar(BuildContext context) {
+// AppBar _buildAppBar(BuildContext context, String title) {
+//   ColorScheme colorScheme = Theme.of(context).colorScheme;
+//   return AppBar(
+//     // prefer: Size.fromHeight(200.0), // Set the height of the AppBar
+
+//     backgroundColor: colorScheme.primary,
+
+//     title: Text(
+//       title,
+//       style: TextStyle(
+//         color: Theme.of(context).colorScheme.onPrimary,
+//         fontSize: 22,
+//         fontWeight: FontWeight.bold,
+//       ),
+//     ),
+//     flexibleSpace: Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             colorScheme.primary,
+//             colorScheme.secondary,
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
+PreferredSize _buildPreferredSize(BuildContext context, String title) {
   ColorScheme colorScheme = Theme.of(context).colorScheme;
-  return AppBar(
-    backgroundColor: colorScheme.primary,
-    title: Text(
-      'App bar',
-      style: TextStyle(
-        color: colorScheme.inversePrimary,
+
+  return PreferredSize(
+    preferredSize: Size.fromHeight(70.0), // Set the height of the AppBar
+    child: ClipRRect(
+      borderRadius: BorderRadius.vertical(
+        bottom: Radius.circular(20.0), // Adjust the radius as needed
+      ),
+      child: AppBar(
+        backgroundColor: colorScheme.primary,
+        title: Padding(
+          padding:
+              const EdgeInsets.only(top: 10.0, left: 10.0), // Adjust as needed
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontSize: 22,
+              fontFamily: 'Kantumruy Pro', // Specify your font family
+              fontWeight: FontWeight.w700, // Use FontWeight.w700 for bold
+            ),
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                colorScheme.primary,
+                colorScheme.secondary,
+              ],
+            ),
+          ),
+        ),
       ),
     ),
   );
--- lib/pages/therapists_page.dart
+++ lib/pages/therapists_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 
 @RoutePage(name: 'TherapistsPage')
+// @RoutePage()
 
 class TherapistsPage extends StatelessWidget {
   const TherapistsPage({super.key});
