8236e460f141786b9bf029f1e7f89c39c0b7e06b
RethtihpongEM <rethtihpong@gmail.com>
2024-06-05 01:10:49 +0700

    fix chhay's feature by pong

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -4,29 +4,10 @@ import 'package:capstone_project_mobile/constants/api_route_constant.dart';
 import 'package:capstone_project_mobile/model/dto/create_appointment.dart';
 import 'package:capstone_project_mobile/model/dto/create_post.dart';
 import 'package:capstone_project_mobile/services/http_service.dart';
-
-Future createPost(CreatePost body) async {
-  HttpService httpService = HttpService(path: ApiRoute.posts.name);
-
-  var HttpResponse(:httpRes, :jsonData) = await httpService.httpPost(
-    body: jsonEncode({
-      'body': body.body,
-      'patient': body.patient,
-      'postPhotos': body.postPhotos,
-    }),
-  );
-
-  if (httpRes.statusCode == 201) {
-    return httpRes;
-  } else {
-    throw jsonData;
-  }
-}
+import 'package:flutter/material.dart';
 
 Future createAppointment(CreateAppointment body) async {
   HttpService httpService = HttpService(path: ApiRoute.appointments.name);
-
-  print(body.getNote);
 
   var HttpResponse(:httpRes, :jsonData) = await httpService.httpPost(
     body: jsonEncode({
@@ -44,10 +25,6 @@ Future createAppointment(CreateAppointment body) async {
     throw jsonData;
   }
 }
-import 'package:capstone_project_mobile/constants/api_route_constant.dart';
-import 'package:capstone_project_mobile/model/dto/create_post.dart';
-import 'package:capstone_project_mobile/services/http_service.dart';
-import 'package:flutter/material.dart';
 
 Future createPost(CreatePost body) async {
   HttpService httpService = HttpService(path: ApiRoute.posts.name);
