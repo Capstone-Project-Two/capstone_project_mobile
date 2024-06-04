babda24b79348aa95394a9bd952ab00d638c0545
RUiNe4 <sunchhay@gmail.com>
2024-06-04 16:00:30 +0700

    CPM-34-update-http-service

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -4,6 +4,13 @@ import 'package:capstone_project_mobile/constants/env_constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
+class HttpMethod {
+  static const String getMethod = 'GET';
+  static const String postMethod = 'POST';
+  static const String patchMethod = 'PATCH';
+  static const String deleteMethod = 'DELETE';
+}
+
 class HttpResponse {
   final Response httpRes;
   final dynamic jsonData;
@@ -14,10 +21,58 @@ class HttpResponse {
   });
 }
 
-Future<HttpResponse> httpGet({required String path}) async {
-  var url = Uri.http(baseApiUrl, '/$path');
-  var res = await http.get(url);
-  var jsonData = jsonDecode(res.body);
+class HttpService {
+  final String path;
 
-  return HttpResponse(jsonData: jsonData, httpRes: res);
+  HttpService({
+    required this.path,
+  });
+
+  static const headers = <String, String>{
+    'Content-Type': 'application/json',
+  };
+
+  get url => Uri.http(baseApiUrl, '/$path');
+
+  Future<HttpResponse> httpGet() async {
+    var res = await http.get(url);
+    var jsonData = jsonDecode(res.body);
+
+    return HttpResponse(jsonData: jsonData, httpRes: res);
+  }
+
+  Future<HttpResponse> httpPost({required dynamic body}) async {
+    var res = await http.post(
+      url,
+      body: body,
+      headers: headers,
+    );
+
+    var jsonData = jsonDecode(res.body);
+
+    return HttpResponse(jsonData: jsonData, httpRes: res);
+  }
+
+  Future<HttpResponse> httpPatch({dynamic body}) async {
+    var res = await http.patch(
+      url,
+      body: body,
+      headers: headers,
+    );
+
+    var jsonData = jsonDecode(res.body);
+
+    return HttpResponse(jsonData: jsonData, httpRes: res);
+  }
+
+  Future<HttpResponse> httpDelete({dynamic body}) async {
+    var res = await http.delete(url, body: body, headers: headers);
+
+    var jsonData = jsonDecode(res.body);
+
+    return HttpResponse(
+      jsonData: jsonData,
+      httpRes: res,
+    );
+  }
 }
